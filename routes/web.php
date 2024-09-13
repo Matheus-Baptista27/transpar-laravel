16b6c5aae75b1f23a2bbf33a508d9bc56e616d3d
Matheus_Baptista <136763434+Matheus-Baptista27@users.noreply.github.com>
2024-09-13 17:37:28 -0300

    niveis de acesso no login

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Backend\AdminController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -18,3 +19,8 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+//rota admin
+Route::get('admin/dashboard', [AdminController::class, 'dashboard'])
+     ->middleware(['auth', 'admin'])
+     ->name('admin.dashboard');
