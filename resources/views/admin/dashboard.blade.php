99827b60487e8836a97661f6c486a90084961c3f
Matheus_Baptista <136763434+Matheus-Baptista27@users.noreply.github.com>
2024-09-16 17:43:20 -0300

    painel de controle admin

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,29 +1,73 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Painel Administrador</title>
-</head>
-<body>
-    <h1>Painel Administrador</h1>
-    <p>{{ Auth::user()->name }}</p>
+@extends('admin.layouts.master')
 
-        <x-dropdown-link :href="route('profile.edit')">
-            {{ __('Meus Dados') }}
-        </x-dropdown-link>
+@section('content')
+<section class="section">
+    <div class="section-header">
+      <h1>Painel de Controle</h1>
+    </div>
 
-        <!-- Authentication -->
-        <form method="POST" action="{{ route('logout') }}">
-            @csrf
+    <div class="row">
+      <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+        <div class="card card-statistic-1">
+          <div class="card-icon bg-primary">
+            <i class="far fa-user"></i>
+          </div>
+          <div class="card-wrap">
+            <div class="card-header">
+              <h4>Total Admin</h4>
+            </div>
+            <div class="card-body">
+              10
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+        <div class="card card-statistic-1">
+          <div class="card-icon bg-danger">
+            <i class="far fa-newspaper"></i>
+          </div>
+          <div class="card-wrap">
+            <div class="card-header">
+              <h4>News</h4>
+            </div>
+            <div class="card-body">
+              42
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+        <div class="card card-statistic-1">
+          <div class="card-icon bg-warning">
+            <i class="far fa-file"></i>
+          </div>
+          <div class="card-wrap">
+            <div class="card-header">
+              <h4>Reports</h4>
+            </div>
+            <div class="card-body">
+              1,201
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 col-12">
+        <div class="card card-statistic-1">
+          <div class="card-icon bg-success">
+            <i class="fas fa-circle"></i>
+          </div>
+          <div class="card-wrap">
+            <div class="card-header">
+              <h4>Online Users</h4>
+            </div>
+            <div class="card-body">
+              47
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
 
-            <x-dropdown-link :href="route('logout')"
-                    onclick="event.preventDefault();
-                                this.closest('form').submit();">
-                {{ __('Sair') }}
-            </x-dropdown-link>
-        </form>
-
-</body>
-</html>
+  </section>
+@endsection
