91f0eb1d37ddc26cab15d21ce76e071eb0eff08e
Matheus_Baptista <136763434+Matheus-Baptista27@users.noreply.github.com>
2024-09-19 16:03:33 -0300

    add img ao profile

--- app/Http/Controllers/Backend/ProfileController.php
+++ app/Http/Controllers/Backend/ProfileController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+use File;
 
 class ProfileController extends Controller
 {
@@ -22,9 +23,28 @@ class ProfileController extends Controller
         $request->validate([
             'name' => ['required', 'max:100'],
             'email' => ['required', 'email', 'unique:users,email,' . Auth::user()->id],
+            'image' => ['image', 'max:2048'],
         ]);
 
         $user = Auth::user();
+        if($request->hasFile('image')) {
+
+
+           //VERIFICA SE JÁ EXISTE A IMAGEM E APAGA PARA NAO ENCHER A PASTA
+           if(File::exists(public_path($user->image))) {
+              File::delete(public_path($user->image));
+           }
+
+            $image = $request->image;
+            $imageName = rand() . '-transpar-' . $image->getClientOriginalName();
+            $image->move(public_path('uploads'), $imageName);
+
+            //CAMINHO DA PASTA DE IMAGENS
+            $path = "/uploads/" . $imageName;
+
+            $user->image = $path;
+        }
+
         $user->name = $request->name;
         $user->email = $request->email;
         $user->save();
--- routes/web/admin.php
+++ routes/web/admin.php
@@ -6,11 +6,15 @@ use Illuminate\Support\Facades\Route;
 
 //ROTA ADMIN
 Route::get('admin/dashboard', [AdminController::class, 'dashboard'])
-     ->middleware(['auth', 'admin'])
-     ->name('admin.dashboard');
+      ->middleware(['auth', 'admin'])
+      ->name('admin.dashboard');
 
 //ROTA ADMIN VER PERFIL
-Route::get('admin/profile', [ProfileController::class, 'index'])->name('admin.profile');
+Route::get('admin/profile', [ProfileController::class, 'index'])
+      ->middleware(['auth', 'admin'])
+      ->name('admin.profile');
 
 //ROTA ADMIN PARA ATUALIZAR DADOS DO PERFIL
-Route::post('admin/profile/update', [ProfileController::class, 'update'])->name('admin.profile.update');
+Route::post('admin/profile/update', [ProfileController::class, 'update'])
+      ->middleware(['auth', 'admin'])
+      ->name('admin.profile.update');
